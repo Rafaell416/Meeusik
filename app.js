3c423b31b0045780b22bbd841c536d078e665261
Rafael Villarreal <rvillarreal416@gmail.com>
2017-05-14 22:32:04 -0500

    artistbox reusable

--- app.js
+++ app.js
@@ -13,35 +13,14 @@ import {
   Image
 } from 'react-native'
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import ArtistBox from './artistBox'
 
 export default class Meeusik extends Component {
 
   render() {
-
-    const image = require('./assets/skillet.png')
-    const name = 'Skillet'
-    const likes = 200
-    const comments = 10
-
     return (
       <View style={styles.container}>
-        <View style={styles.artistBox}>
-          <Image style={styles.image} source={image}/>
-            <View style={styles.info}>
-              <Text style={styles.name}>{name}</Text>
-                <View style={styles.row}>
-                  <View style={styles.iconContainer}>
-                    <Icon name="ios-heart-outline" size={30} color="gray" />
-                    <Text style={styles.count}>{likes}</Text>
-                  </View>
-                   <View style={styles.iconContainer}>
-                    <Icon name="ios-text-outline" size={30} color="gray" />
-                    <Text style={styles.count}>{comments}</Text>
-                  </View>
-                </View>
-            </View>
-        </View> 
+       <ArtistBox />
       </View>
     )
   }
@@ -52,37 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 20
-  },
-  image: {
-    width: 150,
-    height: 150
-  },
-  artistBox:{
-    backgroundColor: 'white',
-    flexDirection: 'row'
-  },
-  info:{
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center'
-  },
-  name: {
-    fontSize: 20,
-    marginTop: 10
-  },
-  row: {
-    flexDirection: 'row',
-    marginTop: 15,
-    marginHorizontal: 30,
-    justifyContent: 'space-between'
-  },
-  iconContainer: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  count: {
-    color: 'gray'
   }
 
 })
---
+++ artistBox.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native'
+
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default class ArtistBox extends Component {
+
+  render() {
+
+    const image = require('./assets/skillet.png')
+    const name = 'Skillet'
+    const likes = 200
+    const comments = 10
+
+    return (
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={image}/>
+            <View style={styles.info}>
+              <Text style={styles.name}>{name}</Text>
+                <View style={styles.row}>
+                  <View style={styles.iconContainer}>
+                    <Icon name="ios-heart-outline" size={30} color="gray" />
+                    <Text style={styles.count}>{likes}</Text>
+                  </View>
+                   <View style={styles.iconContainer}>
+                    <Icon name="ios-text-outline" size={30} color="gray" />
+                    <Text style={styles.count}>{comments}</Text>
+                  </View>
+                </View>
+            </View>
+        </View> 
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150
+  },
+  artistBox:{
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  info:{
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 15,
+    marginHorizontal: 30,
+    justifyContent: 'space-between'
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
+  }
+
+})
+
+
