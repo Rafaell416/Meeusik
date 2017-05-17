4bf1d0d3b2ee381b4e336153c0e24df550b79263
Rafael Villarreal <rvillarreal416@gmail.com>
2017-05-17 02:31:10 -0500

    login view added and ready for FB login

---
+++ src/loginView.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native'
+
+import FBSDK, {
+  LoginButton,
+  AccessToken
+} from 'react-native-fbsdk'
+
+
+export default class LoginView extends Component {
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Bienvenido a Meeusik
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50
+  }
+})
