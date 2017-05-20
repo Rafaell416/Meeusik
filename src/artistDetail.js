0abe839ece8d346febdb11265897be97ed0f89dd
Rafael Villarreal <rvillarreal416@gmail.com>
2017-05-20 02:43:11 -0500

    input for comment added

--- src/artistDetail.js
+++ src/artistDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
+  TextInput
 } from 'react-native'
 
 
@@ -16,6 +17,13 @@ export default class ArtistDetail extends Component {
     return (
       <View style={styles.container}>
         <ArtistBox artist={artist} />
+        <View style={styles.inputContainer}>
+          <TextInput
+          style={styles.input}
+          placeholder="Type here to comment!"
+          onChangeText={(text) => this.setState({text})}
+          />
+        </View>
       </View>
     )
   }
@@ -26,5 +34,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 70
+  },
+  inputContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    height: 50,
+    backgroundColor: 'white',
+    paddingHorizontal: 10
+  },
+  input: {
+    height: 50
   }
 })
